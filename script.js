822bad6fa9f503006c16ec253cfce7c1b5636482
NayanDev7 <semwalnayan7@gmail.com>
2025-05-09 01:04:58 +0530

    created page 6,7 and page7 animations

--- script.js
+++ script.js
@@ -19,34 +19,67 @@ function navAnimation() {
   });
 }
 
-function page3VideoAnimation(){
-    const page3Center = document.querySelector(".page3-center");
+function page3VideoAnimation() {
+  const page3Center = document.querySelector(".page3-center");
 
-const page3Video = document.querySelector("#page3 video");
+  const page3Video = document.querySelector("#page3 video");
 
-page3Center.addEventListener("click" , function(){
+  page3Center.addEventListener("click", function () {
     page3Video.play();
     page3Video.style.transform = "scaleX(1) scaleY(1)";
     // page3Video.style.transform = "scaleY(1)";
     page3Video.style.opacity = "1";
     page3Video.style.borderRadius = "0";
     page3Video.style.transition = "1s ease-in";
-})
+  });
 
-page3Video.addEventListener("click",function(e){
-    // e.stopPropagation();  
+  page3Video.addEventListener("click", function (e) {
+    // e.stopPropagation();
     console.log("hello");
     page3Video.pause();
+    page3Video.currentTime = 0;
     page3Video.style.transform = "scaleX(0.7) scaleY(0)";
     // page3Video.style.transform = "";
     page3Video.style.opacity = "0";
     page3Video.style.borderRadius = "30px";
     page3Video.style.transition = "1s ease-in";
-})
+  });
 }
 
-// navAnimation();
+function page7() {
+
+  const page7SecRight = document.querySelector(".sec-right");
+  const page7SecRightVideo = document.querySelector(".sec-right video");
+
+  page7SecRight.addEventListener("mouseenter", function () {
+    page7SecRightVideo.play();
+    page7SecRightVideo.style.opacity = "1";
+    page7SecRightVideo.style.zIndex = 1;
+  });
+  page7SecRightVideo.addEventListener("mouseleave", function () {
+    page7SecRightVideo.pause();
+    page7SecRightVideo.currentTime = 0;
+    page7SecRightVideo.style.opacity = "0";
+  });
+
+  const page7Sec2Right = document.querySelector(".section2");
+  const page7Sec2RightVideo = document.querySelector(".section2 video");
+
+  page7Sec2Right.addEventListener("mouseover", function () {
+    console.log("h");
+    page7Sec2RightVideo.play();
+  });
+
+  page7Sec2RightVideo.addEventListener("mouseout", function () {
+    page7Sec2RightVideo.pause();
+    page7Sec2RightVideo.currentTime = 0;
+  });
+}
+
+
+
+navAnimation();
+
 page3VideoAnimation();
 
-
-const page5Container = document.querySelector("")
+page7();
