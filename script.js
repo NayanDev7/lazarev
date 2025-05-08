372089e75860528bb7d3f4087873ec1cf2667c2a
NayanDev7 <semwalnayan7@gmail.com>
2025-05-08 17:34:48 +0530

    created page3,4,our-client

--- script.js
+++ script.js
@@ -18,4 +18,35 @@ function navAnimation() {
     });
   });
 }
-navAnimation();
+
+function page3VideoAnimation(){
+    const page3Center = document.querySelector(".page3-center");
+
+const page3Video = document.querySelector("#page3 video");
+
+page3Center.addEventListener("click" , function(){
+    page3Video.play();
+    page3Video.style.transform = "scaleX(1) scaleY(1)";
+    // page3Video.style.transform = "scaleY(1)";
+    page3Video.style.opacity = "1";
+    page3Video.style.borderRadius = "0";
+    page3Video.style.transition = "1s ease-in";
+})
+
+page3Video.addEventListener("click",function(e){
+    // e.stopPropagation();  
+    console.log("hello");
+    page3Video.pause();
+    page3Video.style.transform = "scaleX(0.7) scaleY(0)";
+    // page3Video.style.transform = "";
+    page3Video.style.opacity = "0";
+    page3Video.style.borderRadius = "30px";
+    page3Video.style.transition = "1s ease-in";
+})
+}
+
+// navAnimation();
+page3VideoAnimation();
+
+
+const page5Container = document.querySelector("")
